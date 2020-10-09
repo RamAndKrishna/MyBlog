8495bd6a3c4c5b6e0690c82b385a26d63ba29ec8
ramakrishna <ramakrishna8554@gmail.com>
2020-10-09 23:43:00 +0530

    remove boilerplate articles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Ramakrishna Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Ramakrishna Reddy`,
+      summary: `who lives in Hyderabad, working in bringing ideas to life`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `_RamAndKrishna_`,
     },
   },
   plugins: [
